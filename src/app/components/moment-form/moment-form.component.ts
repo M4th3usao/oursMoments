2135a138aabf4d3850cef113129704a73c70e36a
Matheus Henrique <matheus.rico17@gmail.com>
2022-11-16 08:49:15 -0300

    validações de form com reactive Forms

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,9 +9,33 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MomentFormComponent implements OnInit {
   @Input() btnText!: string 
 
+  momentForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({ //inicializou o form e sues atributos
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),//validação de requisição
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+
+  submit(){
+    if(this.momentForm.invalid){
+      return;
+    }//caso o form seja inválido, ele não ira enviar ao banco
+    console.log('Enviou o formulário')
   }
 
 }
